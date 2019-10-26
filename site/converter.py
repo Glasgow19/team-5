9989a5ec1f9542685b4ab837ec28261125974697
Apolexian <ivananikitin98@gmail.com>
2019-10-26 23:05:43 +0100

    readability

--- site/converter.py
+++ site/converter.py
@@ -3,14 +3,17 @@ import json
 
 # READ THE EXCEL FILE
 df = pd.read_excel("db.xlsx")
+
 # COLUMNS TO REMOVE
 cols=["Alternative exhibit names","Visitor experience","Visitor interaction tips",
 "Good to know","On floor?","Photo location","Photo 1 filename","Photo 1 link",
 "Photo 2 filename","Photo 2 link"]
+
 # DROP THE COLUMNS
 df.drop(cols, axis=1, inplace=True)
 df.insert(0, 'id', range(0, 0 + len(df)))
 d = df.to_dict(orient='records')
+
 # DUMP to FILE
 with open('db.json', 'w', encoding='utf-8') as f:
     json.dump(d, f, ensure_ascii=False, indent=4)
