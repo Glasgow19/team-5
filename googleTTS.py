ef4f1aae964df9b6e2fdc35a809b1d37fc372019
Konstantin Kosev <kkosev97@gmail.com>
2019-10-27 05:10:52 +0000

    Further PythonFlask Commit , with linked pages , qr code working , google TTS working dynamically

--- googleTTS.py
+++ googleTTS.py
@@ -1,11 +1,12 @@
 from google.cloud import texttospeech
+import base64
 import os
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'CS317-GroupProject-d32ac6832124.json'
 # Instantiates a client
 client = texttospeech.TextToSpeechClient()
 def tts(str):
     # Set the text input to be synthesized
-    synthesis_input = texttospeech.types.SynthesisInput(text="Welcome to the science centre")
+    synthesis_input = texttospeech.types.SynthesisInput(text=str)
 
     # Build the voice request, select the language code ("en-US") and the ssml
     # voice gender ("neutral")
@@ -20,4 +21,6 @@ def tts(str):
     # Perform the text-to-speech request on the text input with the selected
     # voice parameters and audio file type
     response = client.synthesize_speech(synthesis_input, voice, audio_config)
+    print(response)
+    response = base64.b64encode(response.audio_content)
     return response
--- static/scripts/exhibit.js
+++ static/scripts/exhibit.js
@@ -1,5 +1,15 @@
 if(localStorage.getItem('sight')){
     document.getElementById('disabilities').innerText+="This is friends to visually impaired!"
+    var text = "Exhibition!";
+     text+= document.getElementById('name').innerText;
+     text+= " !Description";
+     text +=document.getElementById('description').innerText;
+    try {
+            sendQuery(text);
+
+} catch(e) {
+    console.log(e);
+}
 }
 if(localStorage.getItem('hearing')){
     document.getElementById('disabilities').innerText+="This is friends to hearing impaired!"
@@ -7,3 +17,33 @@ if(localStorage.getItem('hearing')){
 if(localStorage.getItem('other')){
     document.getElementById('disabilities').innerText+="This is friends to mobility impaired!"
 }
+
+
+function sendQuery(text){
+
+
+        var request = JSON.stringify({text:text});
+        console.log(request);
+        var xhttp = new XMLHttpRequest();
+
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this.responseText);
+                var test = atob(this.responseText);
+                console.log(test);
+                var snd = new Audio("data:audio/mp3;base64," + this.responseText);
+                snd.play().catch(function(error){
+
+                });
+
+
+
+            }
+            else{
+                console.log("No response");
+            }
+        };
+        xhttp.open("POST", "http://localhost:5000/api/tts", true);
+        xhttp.setRequestHeader("Content-Type", "application/json");
+        xhttp.send(request);
+    }
