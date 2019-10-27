f30d49847568b8b8d0f8186d035bc9afc62e5522
Konstantin Kosev <kkosev97@gmail.com>
2019-10-27 03:23:25 +0000

    Further PythonFlask Commit , with linked pages , qr code working

---
+++ googleTTS.py
@@ -0,0 +1,23 @@
+from google.cloud import texttospeech
+import os
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'CS317-GroupProject-d32ac6832124.json'
+# Instantiates a client
+client = texttospeech.TextToSpeechClient()
+def tts(str):
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.types.SynthesisInput(text="Welcome to the science centre")
+
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.types.VoiceSelectionParams(
+        language_code='en-US-Wavenet-F',
+        ssml_gender=texttospeech.enums.SsmlVoiceGender.FEMALE)
+
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.types.AudioConfig(
+        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(synthesis_input, voice, audio_config)
+    return response
